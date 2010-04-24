31f258ef85abfefeea721a3bfb6ef97a55b5d1a5
mat <mat@eee.(none)>
2010-04-24 12:28:46 +1000

    allow terms of length over 1, not just over 3

--- run.sh
+++ run.sh
@@ -20,8 +20,27 @@ cat cheese_tweets.tsv | tweets_over_week.rb | sort -n | uniq -c | clean_whitespa
 cat tweets_over_day.60.nonaggregated | periodic_trending.rb > tweets_over_day.60.periodic_trending.tsv
 cat tweets_over_day.60.nonaggregated | periodic_trending.rb sliding > tweets_over_day.60.periodic_trending.sliding.tsv
 
-# i can has images
+# i can haz images
 generate_images.sh
 
+# general n-gram setup
+let split=num_lines/4
+split -l $split cheese_tweets.tsv
+echo "" >> xad # in case of mad rounding
 
+# 2grams
+cat xaa | emit_ngrams.rb 2 > xaa.2grams &
+cat xab | emit_ngrams.rb 2 > xab.2grams &
+cat xac | emit_ngrams.rb 2 > xac.2grams &
+cat xad | emit_ngrams.rb 2 > xad.2grams &
+sort -m xa[abcd].2grams | uniq -c > terms.2grams.freq
 
+# 3grams
+cat xaa | emit_ngrams.rb 3 > xaa.3grams &
+cat xab | emit_ngrams.rb 3 > xab.3grams &
+cat xac | emit_ngrams.rb 3 > xac.3grams &
+cat xad | emit_ngrams.rb 3 > xad.3grams &
+sort -m xa[abcd].3grams | uniq -c > terms.3grams.freq
+
+# clean up
+rm xa[abcd]*
