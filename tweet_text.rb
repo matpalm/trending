30b6bc8c68cd5014d86a3f9a6ba6b04b333fa26d
mat <matthew.kelcey@gmail.com>
2010-04-24 22:05:49 +1000

    investigation into 2gram highest trend values, moving onto porting to pig next

--- tweet_text.rb
+++ tweet_text.rb
@@ -10,6 +10,7 @@ STDIN.each do |line|
     tweet = JSON::parse(line)
     next unless tweet.has_key? 'text'
     text = tweet['text']
+#    name = tweet['user']['screen_name']
     time = tweet['created_at']
     puts "#{time}\t#{text.downcase.gsub(/\n/,' ').gsub(/\t/,' ').gsub(/\s+/,' ')}"
   rescue Exception => e
