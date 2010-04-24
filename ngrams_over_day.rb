6a5cd984620dc046232cd213cc0134dfc511a375
mat <matthew.kelcey@gmail.com>
2010-04-24 14:23:50 +1000

    ngrams over day

--- ngrams_over_day.rb
+++ ngrams_over_day.rb
@@ -1,13 +1,15 @@
-n#!/usr/bin/env ruby
-raise "usage: ./emit_ngrams.rb NGRAM_SIZE" unless ARGV.length==1
-NGRAM_SIZE = ARGV.first.to_i
+#!/usr/bin/env ruby
+raise "usage: ./emit_ngrams.rb NGRAM_SIZE BUCKET_SIZE_IN_MINUTES" unless ARGV.length==2
+NGRAM_SIZE, BUCKET_SIZE = ARGV.map(&:to_i)
+require 'parse'
 
 def emit tuple
-  puts tuple.join("\t")
+  puts ([@timeslot] + tuple).join("\t")
 end
 
-require 'parse'
 for_each_post_from_stdin do |datetime, tweet| 
+
+  @timeslot = daily_timeslot_for(datetime, BUCKET_SIZE)
 
   terms = tweet.terms
   next if terms.size < NGRAM_SIZE
