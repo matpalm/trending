49e4147162a36496a766eeed474cbca17b128b9d
mat <matthew.kelcey@gmail.com>
2010-04-25 16:19:11 +1000

    remove special n=1 case from algorithm for simpler streaming

--- trend_streaming.rb
+++ trend_streaming.rb
@@ -4,21 +4,15 @@ class TrendStreaming
 
   def initialize
     @n = 0
-    @mean = @stddev = 0.0
-  end
+    @mean = @mean_sqr = @std_dev = 0.0
+   end
 
   def << e
     @n += 1
     n_1 = @n - 1
-    if @n==1
-      @mean = e
-      @mean_sqr = e ** 2
-      @std_dev = 0
-    else
-      @mean =     ((n_1.to_f * @mean) + e ) / @n
-      @mean_sqr = ((n_1.to_f * @mean_sqr) + e**2) / @n
-      @std_dev =  Math.sqrt(((@n * @mean_sqr) - (@n * (@mean**2))) / (@n-1))
-    end
+    @mean =     ((n_1.to_f * @mean) + e ) / @n
+    @mean_sqr = ((n_1.to_f * @mean_sqr) + e**2) / @n
+    @std_dev =  @mean==e ? 0 : Math.sqrt(((@n * @mean_sqr) - (@n * (@mean**2))) / (@n-1))
   end
 
 
