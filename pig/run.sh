a453040499e4f61c07461c9f5d291d4d62e0778c
mat <mat@eee.(none)>
2010-04-28 07:38:22 +1000

    libify the pig support scripts

--- pig/run.sh
+++ pig/run.sh
@@ -1,3 +1,17 @@
 # pig run.sh!
 
-zcat /data/twitter/gardenhose/cheese.*.json.gz | ./tweet_text.rb 2>/dev/null > cheese_tweets.tsv
+source rc
+
+# generate the original dataset
+zcat /data/twitter/gardenhose/cheese.*.json.gz | tweet_text.rb 2>/dev/null > cheese_tweets.tsv
+
+# split into hourly chunks
+# these form the input to the algo
+mkdir chunks
+cat cheese_tweets.tsv | split.rb
+
+# generate script to run pig jobs
+gen_run_pig_version.rb > run_pig_version.sh
+
+# run it!
+run_pig_version.sh
