c2fbe3685447f6b5d65f345da7b2029429c5e33d
mat <matthew.kelcey@gmail.com>
2010-05-26 15:34:14 +1000

    simplifications from switching from aws emr (pig0.3) to cloudera cdh2 (pig0.5)

--- pig/lib/bootstrap_dir.sh
+++ pig/lib/bootstrap_dir.sh
@@ -1,11 +1,13 @@
-set -x 
-mkdir $1
-cd $1
+set -ex 
+if [ -d trending ]; then
+    mv trending trending.`date +%Y%m%d%H%M%S`
+fi 
+mkdir -p trending
+cd trending
 mkdir {model,fft,trending}
-touch model/000 # required for pig local-empty-file bug
-printf "dummy\t0.0\t0.0\t0.0" > trending/dummy # again, pig empty-file bug :(
+touch model/000 
 ln -s ../chunks.full chunks
 cp ../ngram.rb .
 cd ..
-gen_run_pig_version.rb $1 > run_pig_version.sh
+gen_run_pig_version.rb > run_pig_version.sh
 echo "now run: run_pig_version.sh"
--- pig/lib/gen_run_pig_version.rb
+++ pig/lib/gen_run_pig_version.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby 
-raise "usage: gen_run_pig_version.rb worknig_dir" unless ARGV.length==1
 puts "#!/usr/bin/env bash"
 (0..700).each do |i|
   input = sprintf("%03d",i)
   output = sprintf("%03d",i+1)
-  puts "time pig -x local -p root_path=#{ARGV[0]} -p input=#{input} -p output=#{output} trending.pig"
+  puts "time pig -x local -p input=#{input} -p output=#{output} -p piggybankjar=/home/mat/dev/pig/piggybank.jar trending.pig"
 end
