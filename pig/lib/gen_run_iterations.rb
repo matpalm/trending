1919d97c8610a949926cf2777867752ec21a0d3b
mat <matthew.kelcey@gmail.com>
2010-10-29 21:45:26 +1100

    automatically generate the number of iterations based on the number of chunks

--- pig/lib/gen_run_iterations.rb
+++ pig/lib/gen_run_iterations.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
-NUM = ARGV.length==1 ? ARGV.first.to_i : 10
+NUM_CHUNKS = Dir.glob('chunks/*').size
+raise "expected at least one file in chunks dir" unless NUM_CHUNKS > 1
 require "#{File.dirname(__FILE__)}/run_iteration.rb"
 puts "#!/usr/bin/env bash"
 puts "set -ex"
-(0..NUM).each { |i| puts cmd_for_iter(i) }
+(0..NUM_CHUNKS).each { |i| puts cmd_for_iter(i) }
 
